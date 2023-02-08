a08792cdba8ebad09490459c799c51a40a363f21
Th14g000 <119530357+th14g000@users.noreply.github.com>
2023-02-08 09:32:26 -0300

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -6,23 +6,29 @@ updatedb
 clear
 
 echo "Atualizando Linux parte 1..."
+echo" "
 apt-get update -y
+echo" "
 echo "Feito!"
  
 sleep 3.5
  
 clear
- 
+
 echo "Atualizando Linux parte 2..."
+echo" "
 apt-get upgrade -y
 apt-get dist-upgrade -y
+echo" "
 echo "Feito!"
 
 sleep 3.5
  
 clear
 
+echo" "
 echo "Instalando ferramenta para pentest em VLAN"
+echo" "
 apt install yersinia
 
 sleep 3.5
@@ -30,22 +36,26 @@ clear
 
 
 echo "Instalando scanner Nuclei"
+echo" "
 apt install nuclei
+echo" "
 echo "Feito!"
 
 sleep 3.5
 clear
  
 echo "Instalando Golang"
+echo" "
 apt install golang-go -y
+echo" "
 echo "Feito!"
 
 sleep 3.5
  
 clear
- 
-echo "Instalando Subfinder [Recon de Subdominios]"
 
+echo "Instalando Subfinder [Recon de Subdominios]"
+echo" "
 sleep 3.5
  
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest -y
@@ -55,7 +65,8 @@ wget https://github.com/projectdiscovery/subfinder/releases/download/v2.4.9/subf
 unzip subfinder_2.4.9_linux_amd64.zip
 chmod +x subfinder
 mv subfinder /bin/
- 
+
+echo" "
 echo "Feito!"
  
 clear
